b140688211c7bbbe8b1c76435488e3669fd209be
vitask <skalenko.v@yandex.ru>
2022-08-21 23:53:49 +0700

    2.3. Способы сортировки. Сортировка List

---
+++ src/main/java/ru/job4j/ListSort.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+import java.util.Comparator;
+import java.util.List;
+
+
+public class ListSort {
+    public static List<String> sort(List<String> data) {
+        return data.stream()
+                .sorted(Comparator.nullsLast(Comparator.reverseOrder()))
+                .toList();
+    }
+}
---
+++ src/test/java/ru/job4j/ListSortTest.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+
+class ListSortTest {
+
+    @Test
+    public void test() {
+        List<String> input = Arrays.asList(null, "a", "b", "c");
+        List<String> result = ListSort.sort(input);
+        List<String> expect = Arrays.asList("c", "b", "a", null);
+        Assertions.assertEquals(expect, result);
+    }
+}
