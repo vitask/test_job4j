ff467046ec0cc9c7cdbc7c988a7230e89b2f4aa9
vitask <skalenko.v@yandex.ru>
2022-08-21 21:54:09 +0700

    first commit

--- src/main/java/ru/job4j/Dictionary.java
+++ src/main/java/ru/job4j/Dictionary.java
@@ -1,5 +1,18 @@
 package ru.job4j;
 
-public class Dictionary {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Dictionary {
+    public static Map<String, List<String>> collectData(String[] strings) {
+        Map<String, List<String>> rsl = new HashMap<>();
+        for (String s : strings) {
+            String firstWord = String.valueOf(s.charAt(0));
+            rsl.putIfAbsent(firstWord, new ArrayList<>());
+            rsl.get(firstWord).add(s);
+        }
+        return rsl;
+    }
 }
