a8ac7e18a874d751f1b89e2de5a39f94db6c97fc
vitask <skalenko.v@yandex.ru>
2022-08-21 23:01:20 +0700

    1.4. Получение компаратора. Метод nullsLast()

---
+++ src/test/java/ru/job4j/NullLastMethodTest.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class NullLastMethodTest {
+
+    @Test
+    public void test() {
+        List<String> list = Arrays.asList("3", "2", "1", null);
+        list.sort(NullLastMethod.nullLast());
+        assertEquals(Arrays.asList("1", "2", "3", null), list);
+    }
+}
