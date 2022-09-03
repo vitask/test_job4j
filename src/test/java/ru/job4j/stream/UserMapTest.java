15f26f65b1301a1860e5a0d3d6391f0de5ab5fd9
vitask <skalenko.v@yandex.ru>
2022-09-04 00:10:22 +0700

    5.3. Промежуточные операции. Метод map(). Map с объектами

---
+++ src/test/java/ru/job4j/stream/UserMapTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserMapTest {
+
+    @Test
+    public void test() {
+        List<Integer> ages = List.of(1, 2);
+        List<UserMap.User> result = UserMap.map(ages);
+        Iterator<UserMap.User> iterator = result.iterator();
+        assertEquals(1, iterator.next().getAge());
+        assertEquals(2, iterator.next().getAge());
+    }
+}
