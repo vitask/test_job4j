6ec6560abd1cbbfd8e8373ec765c9c1f9ecdea15
vitask <skalenko.v@yandex.ru>
2022-09-07 00:17:51 +0700

    2.6. Сборка элементов. Сборка в произвольную реализацию Map

---
+++ src/test/java/ru/job4j/stream/CollectToAnyMapTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CollectToAnyMapTest {
+    @Test
+    public void testContent() {
+        List<Integer> data = List.of(2, 3, 3);
+        Map<Integer, Integer> expect = Map.of(
+                2, 4,
+                3, 9
+        );
+        Map<Integer, Integer> result = CollectToAnyMap.collect(data.stream());
+        assertEquals(expect, result);
+    }
+
+    @Test
+    public void testClassName() {
+        List<Integer> data = List.of();
+        String expectClass = "java.util.LinkedHashMap";
+        String resultClass = CollectToAnyMap.collect(data.stream()).getClass().getName();
+        assertEquals(expectClass, resultClass);
+    }
+}
