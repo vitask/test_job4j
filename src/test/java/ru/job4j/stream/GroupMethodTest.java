9fb4d5d0bf177c44bf940e26a5ab26ca974ad081
vitask <skalenko.v@yandex.ru>
2022-09-08 01:08:42 +0700

    2.1. Группировка элементов. Группировка пользователей по возрасту

---
+++ src/test/java/ru/job4j/stream/GroupMethodTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class GroupMethodTest {
+
+    @Test
+    public void test() {
+        GroupMethod.User u1 = new GroupMethod.User(10, "Ivan");
+        GroupMethod.User u2 = new GroupMethod.User(10, "Boris");
+        GroupMethod.User u3 = new GroupMethod.User(15, "Semen");
+        GroupMethod.User u4 = new GroupMethod.User(20, "Petr");
+        Map<Integer, List<GroupMethod.User>> expect = Map.of(
+                10, List.of(u1, u2),
+                15, List.of(u3),
+                20, List.of(u4)
+        );
+        assertEquals(expect, GroupMethod.groupBy(
+                List.of(u1, u2, u3, u4)
+        ));
+    }
+}
