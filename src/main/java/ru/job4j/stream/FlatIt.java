47628ff5cc62827740ec4ad3b38860b1968fbfba
vitask <skalenko.v@yandex.ru>
2022-09-07 23:54:29 +0700

    1. flatMap для итератора

---
+++ src/main/java/ru/job4j/stream/FlatIt.java
@@ -0,0 +1,22 @@
+package ru.job4j.stream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class FlatIt {
+
+    public static List<Integer> flatten(Iterator<Iterator<Integer>> it) {
+        return Stream.of(it)
+                .flatMap(FlatIt::iteratorToStream)
+                .flatMap(FlatIt::iteratorToStream)
+                .collect(Collectors.toList());
+    }
+
+    private static <T> Stream<T> iteratorToStream(Iterator<T> it) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(it, Spliterator.ORDERED),
+                false);
+    }
+}
---
+++ src/test/java/ru/job4j/stream/FlatItTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.Iterator;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+
+class FlatItTest {
+
+    @Test
+    public void whenIt() {
+        Iterator<Iterator<Integer>> it = List.of(
+                List.of(1).iterator(),
+                List.of(2, 3).iterator()
+        ).iterator();
+        assertThat(FlatIt.flatten(it)).isEqualTo(List.of(1, 2, 3));
+    }
+
+}
