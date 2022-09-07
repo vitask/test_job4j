9bfd96cf3eafe78b55dbe7b76de2f93f0adc741f
vitask <skalenko.v@yandex.ru>
2022-09-08 01:20:24 +0700

    2.1. Сборка элементов. Сборка в List

---
+++ src/test/java/ru/job4j/stream/CollectToListTest.java
@@ -0,0 +1,15 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CollectToListTest {
+
+    @Test
+    public void test() {
+        List<Integer> input = List.of(1, 2);
+        List<Integer> expect = List.of(1, 2);
+        assertEquals(expect, CollectToList.collect(input.stream()));
+    }
+}
