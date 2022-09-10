25452f917632b169fcf610488ddea454a256b661
vitask <skalenko.v@yandex.ru>
2022-09-10 23:30:55 +0700

    6.1. Промежуточные операции. Метод filter(). Фильтрация чисел

---
+++ src/main/java/ru/job4j/stream/FilterNumbers.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FilterNumbers {
+
+    public static List<Integer> filter(List<Integer> data) {
+        return data.stream()
+                .filter(i -> i % 2 == 0)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/ru/job4j/stream/FilterNumbersTest.java
@@ -0,0 +1,13 @@
+package ru.job4j.stream;
+
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FilterNumbersTest {
+
+    @Test
+    public void test() {
+        assertEquals(List.of(2, 4), FilterNumbers.filter(List.of(1, 2, 3, 4)));
+    }
+}
