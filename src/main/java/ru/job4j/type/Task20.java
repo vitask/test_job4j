25b4528ae75065c9691ef18b942855e7db9157a6
vitask <skalenko.v@yandex.ru>
2022-09-14 00:17:33 +0700

    20. Разделить двузначное число на отдельные цифры

---
+++ src/main/java/ru/job4j/type/Task20.java
@@ -0,0 +1,11 @@
+package ru.job4j.type;
+
+public class Task20 {
+
+    public static void separate(int number) {
+        int num1, num2;
+        num1 = number / 10;
+        num2 = number % 10;
+        System.out.println(num1 + " " + num2);
+    }
+}
