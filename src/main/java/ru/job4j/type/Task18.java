b49e8da30a7afb11661cd50276124da338aa0c7c
vitask <skalenko.v@yandex.ru>
2022-09-14 00:08:10 +0700

    18. Сумма баллов под крышкой

---
+++ src/main/java/ru/job4j/type/Task18.java
@@ -0,0 +1,8 @@
+package ru.job4j.type;
+
+public class Task18 {
+
+    public static int capSum(int tens, int hundreds, int thousands) {
+        return tens * 10 + hundreds * 100 + thousands * 1000;
+    }
+}
---
+++ src/test/java/ru/job4j/type/Task18Test.java
@@ -0,0 +1,36 @@
+package ru.job4j.type;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class Task18Test {
+
+    @Test
+    public void when1And2And3Then3210() {
+        int rsl = Task18.capSum(1, 2, 3);
+        int expected = 3210;
+        assertThat(rsl).isEqualTo(expected);
+    }
+
+    @Test
+    public void whenAllZeros() {
+        int rsl = Task18.capSum(0, 0, 0);
+        int expected = 0;
+        assertThat(rsl).isEqualTo(expected);
+
+    }
+
+    @Test
+    public void when0And1And0Then100() {
+        int rsl = Task18.capSum(0, 1, 0);
+        int expected = 100;
+        assertThat(rsl).isEqualTo(expected);
+    }
+
+    @Test
+    public void when6And2And1Then1260() {
+        int rsl = Task18.capSum(6, 2, 1);
+        int expected = 1260;
+        assertThat(rsl).isEqualTo(expected);
+    }
+}
