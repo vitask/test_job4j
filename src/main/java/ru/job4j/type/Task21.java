2bc67ea49aa8338e02aa96d70bf5aa85213eb1c6
vitask <skalenko.v@yandex.ru>
2022-09-14 00:22:51 +0700

    21. Разделить трехзначное число на отдельные цифры

---
+++ src/main/java/ru/job4j/type/Task21.java
@@ -0,0 +1,12 @@
+package ru.job4j.type;
+
+public class Task21 {
+
+    public static void separate(int number) {
+        int num1, num2, num3;
+        num1 = number / 100;
+        num2 = number / 10 % 10;
+        num3 = number % 10;
+        System.out.println(num1 + " " + num2 + " " + num3);
+    }
+}
